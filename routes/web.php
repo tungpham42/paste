210bbff7cedb4de84ff83d3330e1e8124cf42b16
Tung Pham <tung.42@gmail.com>
2026-02-27 13:24:58 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PasteController;
 use App\Http\Controllers\Auth\GoogleOneTapController;
+use App\Http\Controllers\SitemapController;
 
 // -----------------------------------------------------------------------------
 // Public Routes
