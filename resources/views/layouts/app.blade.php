3582b7aff0915d0338acbda1b3f1ef6dcc1d683d
Tung Pham <tung.42@gmail.com>
2026-02-27 12:57:02 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -66,6 +66,7 @@
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
+                        'Accept': 'application/json',
                         'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
                     },
                     body: JSON.stringify({ credential: response.credential })
